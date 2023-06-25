5ea6d9407351564a346c0bc735bd899f571f427d
Melikyan Rafael <melikyanrafael13@gmail.com>
2023-06-25 23:46:25 +0300

    Стал более приятный фильтр, также стал глобален axios

--- src/components/CatsApp.vue
+++ src/components/CatsApp.vue
@@ -7,6 +7,7 @@
             v-show="!showFilter"
         >
             <v-btn text @click="IsShowFilter">
+                <v-icon>mdi-filter</v-icon>
                 Фильтр
             </v-btn>
         </v-col>
@@ -15,16 +16,18 @@
             class="mt-5"
             v-show="showFilter"
         >
-            <v-card class="text-center" flat height="50">
+            <v-card flat class="text-right">
+                <v-icon @click="IsShowFilter">mdi-close</v-icon>
+            </v-card>
+            <v-card class="text-center " flat height="20">
                 <span class="grey--text">Фильтр</span>
             </v-card>
             <v-list dense>
-                <v-subheader class="subtitle-1">Выберите возраст кошки:</v-subheader>
-                <v-radio-group row class="mt-1">
+                <v-subheader class="subtitle-1">Выберите пол кошки:</v-subheader>
+                <v-radio-group row class="mt-1" v-model="filters.sex" mandatory>
                     <v-radio class="mb-2" label="Без разницы" value="1"></v-radio>
-                    <v-radio class="mb-2" label="Котята" value="2"></v-radio>
-                    <v-radio class="mb-2" label="Молодые кошки" value="3"></v-radio>
-                    <v-radio class="mb-2" label="Старые кошки" value="4"></v-radio>
+                    <v-radio class="mb-2" label="Девочка" value="2"></v-radio>
+                    <v-radio class="mb-2" label="Мальчик" value="3"></v-radio>
                 </v-radio-group>
                 <!-- <v-list-item>
                     <v-list-item-content>
@@ -55,13 +58,20 @@
                     
                 </v-list-item-group> -->
                 <v-subheader class="subtitle-1">Выберите возраст кошки:</v-subheader>
-                <v-radio-group row class="mt-1">
+                <v-radio-group row class="mt-1" v-model="filters.age" mandatory>
                     <v-radio class="mb-2" label="Без разницы" value="1"></v-radio>
                     <v-radio class="mb-2" label="Котята" value="2"></v-radio>
                     <v-radio class="mb-2" label="Молодые кошки" value="3"></v-radio>
                     <v-radio class="mb-2" label="Старые кошки" value="4"></v-radio>
                 </v-radio-group>
+                <v-row justify="center">
+                    <v-btn dark small right @click="ResetFilter">
+                        <v-icon>mdi-filter-off</v-icon>
+                        Сбросить фильтр
+                    </v-btn>
+                </v-row>
             </v-list>
+            
         </v-col>
         <v-divider vertical v-show="showFilter"></v-divider>
         <v-col
@@ -89,13 +99,31 @@ export default {
     data() {
         return {
             showFilter: false,
-            col: 12
+            col: 12,
+            filters: {
+                sex: 1,
+                age: 1
+            }
         }
+    },
+    mounted() {
+        this.$http.get('http://localhost:3000/products')
+            .then(response => {
+                console.log(response)
+            })
+            .catch(err => {
+                console.log(err)
+            })
     },
     methods: {
         IsShowFilter() {
             this.showFilter = !this.showFilter
             this.col = this.showFilter ? 10 : 12
+        },
+        ResetFilter() {
+            for (let key in this.filters) {
+                this.filters[key] = 1
+            }
         }
     }
 }
